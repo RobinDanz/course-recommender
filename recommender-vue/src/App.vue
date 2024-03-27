f2ca6076f712d4ad913cd7d1c0f6f34f227533d4
Robin Danz <robin.danz@gmail.com>
2024-03-27 17:31:02 +0100

    init vue project

---
+++ recommender-vue/src/App.vue
@@ -0,0 +1,31 @@
+<script setup lang="ts">
+import Menubar from 'primevue/menubar'
+import { ref } from 'vue'
+
+const items = ref([
+  {
+    label: 'Home',
+    icon: 'pi pi-home',
+    route_name: 'home'
+  },
+  {
+    label: 'Features',
+    icon: 'pi pi-star',
+    route_name: 'about'
+  }
+])
+</script>
+
+<template>
+  <header>
+    <Menubar :model="items">
+      <template></template>
+    </Menubar>
+  </header>
+
+  <main>
+    <RouterView></RouterView>
+  </main>
+</template>
+
+<style scoped></style>
