f100520ff9f60025f14c426bcd3447de5cdc103e
michelefischer <michele.fischer@unine.ch>
2024-04-18 12:24:21 +0200

    update

--- recommender-server/src/controllers/form_controller.py
+++ recommender-server/src/controllers/form_controller.py
@@ -2,6 +2,9 @@ from models.form import *
 
 
 def fuzzy(form: FormRequest):
+    """
+    test
+    """
     if form.a > 10:
         return FormResponse(response='a>10')
     return FormResponse(response='a<=10')
